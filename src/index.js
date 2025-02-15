1e7ef4fb884acaafb87a8cb10fb0b91f00b1ce72
zoya5636 <83867635+zoya5636@users.noreply.github.com>
2025-02-15 15:48:52 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import SearchBooks from './components/SearchBooks/SearchBooks'
 import BookCard from './components/BookCard/BookCard'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <SearchBooks></SearchBooks>
+    /* <App /> */
   </React.StrictMode>
 );
 
