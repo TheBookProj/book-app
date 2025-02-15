4797438a0ec3fa00973424f93f8fa95a0d52aab6
zoya5636 <83867635+zoya5636@users.noreply.github.com>
2025-02-15 15:49:24 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import BookCard from './components/BookCard/BookCard'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
